0d2436f082028390017a2d9194e7023b04c5e89b
Santos Martín García <santos.martingarca2@gmail.com>
2016-09-29 10:12:06 -0600

    implementacion de css y vista base

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def post_list(request):
     posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail (request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
